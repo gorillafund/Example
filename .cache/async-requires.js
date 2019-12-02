64c5f680633539ef0776b821aed09a15fec0a8a8
Colt21 <Jakob.Sievers@firma-21.de>
2019-12-02 11:32:39 +0100

    Edited Readme - first changes

---
+++ .cache/async-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-templates-fund-template-js": () => import("../src/templates/fundTemplate.js" /* webpackChunkName: "component---src-templates-fund-template-js" */),
+  "component---cache-dev-404-page-js": () => import("dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-templates-fund-template-js": hot(preferDefault(require("/home/colt/Software/gatsby/Example/src/templates/fundTemplate.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/colt/Software/gatsby/Example/.cache/dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/colt/Software/gatsby/Example/src/pages/index.js")))
+}
+
